fe98fb576ce206a15a866fca768d32fd425dd810
Tal Liron <tal.liron@gmail.com>
2011-12-31 02:40:35 -0600

    mtail!

--- components/plugins/logging-mongodb/libraries/plugins/logging.mongodb.js
+++ components/plugins/logging-mongodb/libraries/plugins/logging.mongodb.js
@@ -1,4 +1,9 @@
 
+document.executeOnce('/mongo-db/')
+document.executeOnce('/savory/templates/')
+document.executeOnce('/savory/objects/')
+document.executeOnce('/savory/localization/')
+
 function getCommands() {
 	return ['mtail']
 }
@@ -12,4 +17,32 @@ function run(command) {
 }
 
 function mtail(command) {
+	var connection = MongoDB.connect()
+	var collection = new MongoDB.Collection('common', {connection: connection, db: 'logs'})
+	var c = collection.find().addOption('tailable').addOption('awaitData')
+	
+	var format = new Savory.Localization.DateTimeFormat('yyy-MM-dd HH:mm:ss.SSS')
+	
+	while (true) {
+		var record = c.next()
+		record = Savory.Objects.flatten(record)
+		record.timestamp = format.format(record.timestamp)
+		while (record.level.length < 5) {
+			record.level += ' '
+		}
+		/*
+		record.timestamp
+		record.level
+		record.thread
+		record.host.process
+		record.message
+		record.loggerName.fullyQualifiedClassName
+		record.filename
+		recrod.lineNumber
+		record.method
+		record['class'].fullyQualifiedClassName
+		*/
+		var line = '{timestamp}: {level} [{loggerName.fullyQualifiedClassName}] {message}'.cast(record)
+		print(line + '\n')
+	}
 }
