cfde84f7905225cdfe94c7c3d8e8add97f9bb7f5
Tal Liron <tal.liron@gmail.com>
2012-03-01 14:50:40 -0600

    Add jsdoc plugin Add media files

---
+++ components/plugins/jsdoc/libraries/scripturian/plugins/jsdoc.js
@@ -0,0 +1,24 @@
+
+function getCommands() {
+	return ['jsdoc']
+}
+
+function run(command) {
+	switch (String(command.name)) {
+		case 'jsdoc':
+			jsdoc(command)
+			break
+	}
+}
+
+function jsdoc(command) {
+	var run = command.sincerity.container.getLibrariesFile('javascript', 'jsdoc-toolkit', 'app', 'run.js')
+
+	var mainArguments = [run]
+	var arguments = command.arguments
+	for (var i in arguments) {
+		mainArguments.push(arguments[i])
+	}
+	mainArguments.push('-j=' + run)
+	command.sincerity.run('javascript:javascript', mainArguments)
+}
