313b4da9185be21f27ba69e631cc8dc66156ece2
Tal Liron <tal.liron@gmail.com>
2012-03-01 12:05:47 -0600

    Some cleanup and reorganization of files

--- container/instance/default.js
+++
@@ -1,8 +0,0 @@
-
-print(executable.context.adapter.attributes.get('language.name') + '\n')
-
-hi = new Packages.test.Hi()
-hi.say()
-
-logging = sincerity.plugins.get('logging')
-logging.run('log', ['Hello!!!!'], sincerity)
