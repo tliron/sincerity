b55419627afb94983582eea6b1db9c0a01f9f9ff
Tal Liron <tal.liron@gmail.com>
2012-03-15 18:08:01 -0500

    Allow Nexus to coexist with logging plugin

--- components/skeletons/nexus/server/contexts/nexus/default.js
+++ components/skeletons/nexus/server/contexts/nexus/default.js
@@ -1,5 +1,7 @@
 
-importClass(org.eclipse.jetty.webapp.WebAppContext, java.lang.System)
+importClass(
+	org.eclipse.jetty.webapp.WebAppContext,
+	java.lang.System)
 
 System.setProperty('plexus.nexus-work', Sincerity.Container.getFileFromHere('work'))
 
@@ -7,4 +9,5 @@ var context = new WebAppContext()
 context.contextPath = '/'
 context.resourceBase = Sincerity.Container.getFileFromHere('web')
 context.tempDirectory = Sincerity.Container.getFileFromHere('work')
+context.addServerClass('org.slf4j.') // Avoid conflicts with Sincerity's logging plugin
 server.handler.addHandler(context)
