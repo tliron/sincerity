23e50b09159e674c1e941e79d9906fe55a96217a
Tal Liron <tal.liron@gmail.com>
2012-03-17 13:55:43 -0500

    More efficient SortedNode

--- components/sincerity/source/com/threecrickets/sincerity/plugin/gui/SortedNode.java
+++ components/sincerity/source/com/threecrickets/sincerity/plugin/gui/SortedNode.java
@@ -1,11 +1,9 @@
 package com.threecrickets.sincerity.plugin.gui;
 
-import java.util.Collections;
-
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.MutableTreeNode;
 
-public class SortedNode extends DefaultMutableTreeNode implements Comparable<SortedNode>
+public class SortedNode extends DefaultMutableTreeNode
 {
 	//
 	// Construction
@@ -26,20 +24,20 @@ public class SortedNode extends DefaultMutableTreeNode implements Comparable<Sor
 	//
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public void add( MutableTreeNode newChild )
 	{
-		super.add( newChild );
-		Collections.sort( this.children );
-	}
-
-	//
-	// Comparable
-	//
-
-	public int compareTo( SortedNode o )
-	{
-		return toString().compareTo( o.toString() );
+		String string = newChild.toString();
+		int i = 0;
+		if( children != null )
+		{
+			for( Object child : children )
+			{
+				if( string.compareTo( child.toString() ) < 0 )
+					break;
+				i++;
+			}
+		}
+		insert( newChild, i );
 	}
 
 	// //////////////////////////////////////////////////////////////////////////
