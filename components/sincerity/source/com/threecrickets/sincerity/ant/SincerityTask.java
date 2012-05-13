a8d2d8c636d4b3ead0bf2d0cfa821602a58dce0c
Tal Liron <tal.liron@gmail.com>
2012-05-13 14:13:05 -0500

    Initial work on Ant task

---
+++ components/sincerity/source/com/threecrickets/sincerity/ant/SincerityTask.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2011-2012 Three Crickets LLC.
+ * <p>
+ * The contents of this file are subject to the terms of the LGPL version 3.0:
+ * http://www.gnu.org/copyleft/lesser.html
+ * <p>
+ * Alternatively, you can obtain a royalty free commercial license with less
+ * limitations, transferable or non-transferable, directly from Three Crickets
+ * at http://threecrickets.com/
+ */
+
+package com.threecrickets.sincerity.ant;
+
+import java.util.ArrayList;
+
+import org.apache.tools.ant.Task;
+
+import com.threecrickets.sincerity.Sincerity;
+
+/**
+ * Sincerity Ant task.
+ * 
+ * @author Tal Liron
+ */
+public class SincerityTask extends Task
+{
+	//
+	// Task
+	//
+
+	@Override
+	public void execute()
+	{
+		ArrayList<String> arguments = new ArrayList<String>();
+		for( Arg arg : args )
+			arguments.add( arg.value );
+		Sincerity.main( arguments.toArray( new String[arguments.size()] ) );
+	}
+
+	public Arg createArg()
+	{
+		Arg arg = new Arg();
+		args.add( arg );
+		return arg;
+	}
+
+	public static class Arg
+	{
+		public void setValue( String value )
+		{
+			this.value = value;
+		}
+
+		private String value;
+	}
+
+	// //////////////////////////////////////////////////////////////////////////
+	// Private
+
+	private final ArrayList<Arg> args = new ArrayList<Arg>();
+}
