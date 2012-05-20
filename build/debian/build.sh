54521e8f18ce3b41357c2b947e1626f4fbdd2092
Tal Liron <tal.liron@gmail.com>
2012-05-20 17:03:47 -0500

    Licensing fixes

--- build/debian/build.sh
+++ build/debian/build.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#
-# Copyright 2009-2012 Three Crickets LLC.
-#
-# The contents of this file are subject to the terms of the LGPL version 3.0:
-# http://www.opensource.org/licenses/lgpl-3.0.html
-#
-# Alternatively, you can obtain a royalty free commercial license with less
-# limitations, transferable or non-transferable, directly from Three Crickets
-# at http://threecrickets.com/
-#
 
 set -e
 
--- build/rpm/build.sh
+++ build/rpm/build.sh
@@ -1,13 +1,4 @@
-#
-# Copyright 2009-2010 Three Crickets LLC.
-#
-# The contents of this file are subject to the terms of the LGPL version 3.0:
-# http://www.opensource.org/licenses/lgpl-3.0.html
-#
-# Alternatively, you can obtain a royalty free commercial license with less
-# limitations, transferable or non-transferable, directly from Three Crickets
-# at http://threecrickets.com/
-#
+#!/bin/bash
 
 set -e
 
