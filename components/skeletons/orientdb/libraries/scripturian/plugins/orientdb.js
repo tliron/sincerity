baea00a9cd867a896463021f93b7361de941598e
Tal Liron <tal.liron@gmail.com>
2012-08-01 16:05:34 -0500

    OrientDB: code cleanup

--- components/skeletons/orientdb/libraries/scripturian/plugins/orientdb.js
+++ components/skeletons/orientdb/libraries/scripturian/plugins/orientdb.js
@@ -14,54 +14,42 @@ function getCommands() {
 function run(command) {
 	switch (String(command.name)) {
 		case 'server':
-			server(command)
+			orientdb(command, 'com.orientechnologies.orient.server.OServerMain', true)
 			break
 		case 'console':
-			console(command)
+			orientdb(command, 'com.orientechnologies.orient.graph.console.OGremlinConsole')
 			break
 		case 'gremlin':
-			gremlin(command)
+			orientdb(command, 'com.tinkerpop.gremlin.groovy.console.Console')
 			break
 	}
 }
 
-function server(command) {
+function orientdb(command, className, isServer) {
+	var arguments = [className]
+	
+	for (var i in command.arguments) {
+		arguments.push(command.arguments[i])
+	}
 	try {
 		command.sincerity.run('logging:logging')
 	} catch(x) {}
-	properties(command)
-	command.sincerity.run('delegate:main', ['com.orientechnologies.orient.server.OServerMain'])
-}
-
-function console(command) {
-	var arguments = ['com.orientechnologies.orient.graph.console.OGremlinConsole']
-	for (var i in command.arguments) {
-		arguments.push(command.arguments[i])
-	}
-	properties(command)
-	command.sincerity.run('delegate:main', arguments)
-}
-
-function gremlin(command) {
-	var arguments = ['com.tinkerpop.gremlin.groovy.console.Console']
-	for (var i in command.arguments) {
-		arguments.push(command.arguments[i])
-	}
-	properties(command)
-	command.sincerity.run('delegate:main', arguments)
-}
-
-function properties(command) {
-	System.setProperty('ORIENTDB_HOME', sincerity.container.root)
-	System.setProperty('orientdb.config.file', sincerity.container.getConfigurationFile('orientdb', 'server.conf'))
-	System.setProperty('orientdb.www.path', sincerity.container.getFile('web'))
 	
-	var file = command.sincerity.container.getConfigurationFile('orientdb', 'properties.conf')
-	if (file.exists()) {
-		var properties = Sincerity.JVM.loadProperties(file)
-		for (var e = properties.propertyNames(); e.hasMoreElements(); ) {
-			var name = e.nextElement()
-			System.setProperty(name, properties.get(name))
+	System.setProperty('ORIENTDB_HOME', sincerity.container.root)
+	
+	if (isServer) {
+		System.setProperty('orientdb.config.file', sincerity.container.getConfigurationFile('orientdb', 'server.conf'))
+		System.setProperty('orientdb.www.path', sincerity.container.getFile('web'))
+		
+		var file = command.sincerity.container.getConfigurationFile('orientdb', 'properties.conf')
+		if (file.exists()) {
+			var properties = Sincerity.JVM.loadProperties(file)
+			for (var e = properties.propertyNames(); e.hasMoreElements(); ) {
+				var name = e.nextElement()
+				System.setProperty(name, properties.get(name))
+			}
 		}
 	}
+
+	command.sincerity.run('delegate:main', arguments)
 }
