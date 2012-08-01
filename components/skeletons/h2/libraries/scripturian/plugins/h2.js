3dc2b9264a0d5e7617ea68244cc9fbcfc3d6315a
Tal Liron <tal.liron@gmail.com>
2012-08-01 04:58:02 -0500

    H2 skeleton: add all tools

--- components/skeletons/h2/libraries/scripturian/plugins/h2.js
+++ components/skeletons/h2/libraries/scripturian/plugins/h2.js
@@ -4,33 +4,73 @@ function getInterfaceVersion() {
 }
 
 function getCommands() {
-	return ['server', 'console', 'cluster']
+	return [
+		'server',
+		'console',
+		'backup',
+		'change-file-encryption',
+		'compress',
+		'convert-trace-file',
+		'create-cluster',
+		'csv',
+		'delete-db-files',
+		'multi-dimension',
+		'recover',
+		'restore',
+		'run-script',
+		'script',
+		'shell'
+	]
 }
 
 function run(command) {
 	switch (String(command.name)) {
 		case 'server':
-			server(command)
+			h2(command, 'org.h2.tools.Server', 'server')
 			break
 		case 'console':
-			console(command)
+			h2(command, 'org.h2.tools.Console', 'console')
 			break
-		case 'cluster':
-			cluster(command)
+		case 'backup':
+			h2(command, 'org.h2.tools.Backup', 'backup')
+			break
+		case 'change-file-encryption':
+			h2(command, 'org.h2.tools.ChangeFileEncryption', 'change-file-encryption')
+			break
+		case 'compress':
+			h2(command, 'org.h2.tools.CompressTool', 'compress')
+			break
+		case 'convert-trace-file':
+			h2(command, 'org.h2.tools.ConvertTraceFile', 'convert-trace-file')
+			break
+		case 'create-cluster':
+			h2(command, 'org.h2.tools.CreateCluster', 'create-cluster')
+			break
+		case 'csv':
+			h2(command, 'org.h2.tools.Csv', 'csv')
+			break
+		case 'delete-db-files':
+			h2(command, 'org.h2.tools.DeleteDbFiles', 'delete-db-files')
+			break
+		case 'multi-dimension':
+			h2(command, 'org.h2.tools.MultiDimension', 'multi-dimension')
+			break
+		case 'recover':
+			h2(command, 'org.h2.tools.Recover', 'recover')
+			break
+		case 'restore':
+			h2(command, 'org.h2.tools.Restore', 'restore')
+			break
+		case 'run-script':
+			h2(command, 'org.h2.tools.RunScript', 'run-script')
+			break
+		case 'script':
+			h2(command, 'org.h2.tools.Script', 'script')
+			break
+		case 'shell':
+			h2(command, 'org.h2.tools.Shell', 'shell')
 			break
 	}
-}
-
-function server(command) {
-	h2(command, 'org.h2.tools.Server', 'server')
-}
-
-function console(command) {
-	h2(command, 'org.h2.tools.Console', 'console')
-}
-
-function cluster(command) {
-	h2(command, 'org.h2.tools.CreateCluster', 'cluster')
 }
 
 function h2(command, className, confName) {
@@ -52,8 +92,10 @@ function h2(command, className, confName) {
 		}
 	}
 
-	arguments.push('-baseDir')
-	arguments.push(command.sincerity.container.getFile('data'))
+	if ((confName == 'server') || (confName == 'console')) {
+		arguments.push('-baseDir')
+		arguments.push(command.sincerity.container.getFile('data'))
+	}
 
 	for (var i in command.arguments) {
 		arguments.push(command.arguments[i])
