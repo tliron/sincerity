e4d91dfbe190435f3771ce3186db8fa3d13317fd
Tal Liron <tal.liron@gmail.com>
2013-11-06 23:21:38 +0800

    Upgrade to Jetty 9 (requires JVM 7)

--- components/skeletons/jetty-servlet/server/connectors/default.js
+++ components/skeletons/jetty-servlet/server/connectors/default.js
@@ -1,6 +1,18 @@
 
+// Jetty 9
+
+importClass(org.eclipse.jetty.server.ServerConnector)
+
+var connector = new ServerConnector(server)
+connector.port = 8080
+server.addConnector(connector)
+
+/*
+// Jetty 8
+
 importClass(org.eclipse.jetty.server.nio.SelectChannelConnector)
 
 var connector = new SelectChannelConnector()
 connector.port = 8080
 server.addConnector(connector)
+*/
