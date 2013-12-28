7fd51b98fa83c7711aeb058c79a832eec74ff13e
Tal Liron <tal.liron@gmail.com>
2013-12-28 16:17:01 +0800

    Update copyright notice for 2014

--- components/sincerity-javascript-library/libraries/scripturian/sincerity/classes.js
+++ components/sincerity-javascript-library/libraries/scripturian/sincerity/classes.js
@@ -2,7 +2,7 @@
 // This file is part of the Sincerity Foundation Library
 //
 
-// Copyright 2011-2012 Three Crickets LLC.
+// Copyright 2011-2014 Three Crickets LLC.
 //
 // The contents of this file are subject to the terms of the LGPL version 3.0:
 // http://www.gnu.org/copyleft/lesser.html
