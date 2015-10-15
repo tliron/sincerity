cc591b49861480322513935ac9f926015aab39d2
Tal Liron <tal.liron@gmail.com>
2015-10-15 10:23:41 -0500

    Documentation fixes

--- components/skeletons/restlet/component/servers/http.js
+++ components/skeletons/restlet/component/servers/http.js
@@ -33,7 +33,7 @@ var server = new Server(Protocol.HTTP, port)
 server.name = 'default'
 component.servers.add(server)
 
-// Add support for the X-FORWARDED-FOR header used by proxies, such as Apache's
+// Add support for the X-Forwarded-For header used by proxies, such as Apache's
 // mod_proxy. This guarantees that request.clientInfo.upstreamAddress returns
 // the upstream address behind the proxy.
 // server.context.parameters.set('useForwardedForHeader', 'true')
