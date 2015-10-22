207f70fdda4d5bfc8b4ac95de198d60e5447c5ef
Tal Liron <tal.liron@gmail.com>
2015-10-21 19:20:59 -0500

    Sincerity.Classes: improve documentation

--- components/sincerity-javascript-library/libraries/scripturian/sincerity/classes.js
+++ components/sincerity-javascript-library/libraries/scripturian/sincerity/classes.js
@@ -17,26 +17,28 @@ document.require('/sincerity/objects/')
 var Sincerity = Sincerity || {}
 
 /**
- * Object Oriented Programming for JavaScript, Sincerity-style.
+ * Object Oriented Programming (OOP) for JavaScript, Sincerity-style.
  * <p>
  * Though JavaScript does not in itself have most OOP constructs, it provides all the essentials for
- * building an OOP framework. And this is Sincerity's take on it: we've opted for an especially lightweight
- * approach to provide the bare minimum required for an OOP development experience that promotes
- * clarity, consistency and functionality. If you've come from heavily OOP languages, such as Java or
+ * building an OOP framework. For Sincerity, we've opted for an especially lightweight
+ * approach, to provide the bare minimum required for an OOP experience that promotes
+ * clarity, consistency and functionality.
+ * <p>
+ * If you've come from heavily OOP languages, such as Java or
  * Python, we'd like to encourage you to avoid automatically turning everything into a "class," and 
- * instead and choose more horizontal ways of code organization and reuse, via closures, raw prototypes
+ * instead explore more horizontal ways of code organization and reuse, via closures, raw prototypes
  * and duck typing. Use OOP only when it adds coherence to your code.
  * <p>
- * That said, Sincerity supports all OOP principles:
+ * OOP principles supported:
  * <ol>
  * <li>Encapsulation: This is already available, because functions are first-class citizens in JavaScript.
  * A dict can contain both data properties and functions. Additionally, the "this" keyword is automatically
  * linked to a new dict via the "new" keyword. The result is that data and functions are encapsulated
  * into a single instance.</li>
  * <li>Inheritance: There are two obvious ways to implement this in JavaScript -- by simply copying functions
- * over from another dict (which we could have done with {@link Sincerity.Objects#merge}) or at once by
- * setting the constructor's prototype. The latter offers better performance and more coherence, which is
- * why we chose it. Additionally, for all methods, we hook up the overridden member via the ".overridden"
+ * over from another dict (which can be done with {@link Sincerity.Objects#merge}) or by
+ * setting the constructor's prototype. The latter method offers better performance and support for "instanceof".
+ * Additionally, for all methods, we hook up the overridden member via the ".overridden"
  * field in the function object itself. You can access it from inside the method as
  * "arguments.callee.overridden". Note that this also works for the "._construct" constructor.</li>
  * <li>Polymorphism: Loose polymorphism is provided naturally via JavaScript's duck typing, which is
@@ -49,13 +51,32 @@ var Sincerity = Sincerity || {}
  * <p>
  * Another useful feature we support, which has become popular in many OOP implementations, is the ability
  * to annotate classes. Any key in the class definition that begins with "_" is not copied to prototype,
- * but can be accessed via the prototype's "definition" key. 
+ * but can be accessed via the prototype's "definition" key. This allows you to add keys to the class only,
+ * but not to instances.
  * <p>
- * An example, with the module pattern:
+ * We support some optional annotations:
+ * <ol>
+ * <li>_inherit: A parent class. You will inherit all its properties and methods. Additionally, you will inherit its
+ * _configure options (see _configure, below). You can override any property and method in your class. Within a method,
+ * you can access the overridden method using: arguments.callee.overridden.</li>
+ * <li>_construct: Your own custom constructor function. It can take any arguments you wish.
+ * You can call the parent class's constructor using: arguments.callee.overridden.call(this). (This is not done
+ * automatically.) A facility for creating simple constructors is provided by _configure (see below).
+ * If you don't specify _construct or _configure, a default empty constructor will be used.</li>
+ * <li>_configure: A nifty way to automatically generate a constructor that merges properties into the instance.
+ * The value is an array of strings. The generated constructor will accept a single argument, which is a dict.
+ * The array of strings you specified in _configure will be the keys from that dict that will be merged into
+ * the instance. Note that you can still use _construct in this case to do extra construction work! Your _construct
+ * function will be called <i>after</i> the generated constructor, and will receive "this" as its only argument.
+ * Another important note: if you used _inherit, then your _configure array of strings will be concatenated into the
+ * parent _configure array of strings, so that all _configure keys will be merged. If you don't want this behavior,
+ * use _configureOnly (see below).
+ * </li>
+ * <li>_configureOnly: This is identical to _configure, except that the parent class's _configure array will not
+ * be merged into yours.</li>
+ * </ol>
  * <p>
- * (Note that this pattern does not let you create private properties that are <i>not</i> static.
- * Some would argue that this is no great loss, because too much information hiding can be
- * a bad practice. )
+ * An example:
  * <pre>
  * // We are not providing a literal dict, but instead creating an "ad hoc" anonymous function,
  * // which we then immediately call, so that it only ever used this once for purposes of closure
